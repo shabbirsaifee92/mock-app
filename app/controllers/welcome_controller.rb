2f8d79fa79466817d8f6a2c49b8ecfa0a8ff8812
shabbir <ssaifee@covermymeds.com>
2018-05-02 15:12:44 -0400

    call phoenix api from controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,16 @@
+require "base64"
 class WelcomeController < ApplicationController
 
   def index
+    file_content = Base64.encode64(IO.read("#{Rails.root}/app/test.docx"))
+    uri =  URI.parse("http://localhost:4000/api/upload")
+    http = Net::HTTP.new(uri.host, uri.port)
+    data = {
+        content: file_content
+    }
+    req = Net::HTTP::Post.new(uri)
+    req.body = data.to_json
+    res = http.request(req)
+    res.to_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  post '/' => redirect("http://localhost/4001")
+  get '/' => redirect("http://localhost:4000/api/upload")
 end
